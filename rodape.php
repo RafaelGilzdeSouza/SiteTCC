b3114f5f9961814d88e5984018075a55e8c99ae2
RafaelGilzdeSouza <gilzrafa@gmail.com>
2023-11-05 14:02:50 -0300

    rodapé

--- rodape.php
+++ rodape.php
@@ -1,25 +1,25 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Informações Legais</title>
+  <title>Rodapé</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
-  <style>
-    .container {
-      margin-top: 50px;
-    }
-  </style>
 </head>
 <body>
-  <div class="container">
-    <h1>Políticas de Privacidade</h1>
-    <p>Aqui você pode fornecer detalhes sobre suas políticas de privacidade.</p>
-
-    <h1>Termos de Uso</h1>
-    <p>Aqui você pode fornecer detalhes sobre seus termos de uso.</p>
-
-    <h1>Direitos Autorais</h1>
-    <p>Aqui você pode fornecer detalhes sobre os direitos autorais do seu site ou conteúdo.</p>
-  </div>
+<footer class="footer bg-dark text-light fixed-bottom">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <p>&copy; <?php echo date('Y'); ?> Seu Nome ou Empresa</p>
+            </div>
+            <div class="col-md-6">
+                <ul class="list-inline text-center text-md-right">
+                    <li class="list-inline-item"><a href="#">Política de Privacidade</a></li>
+                    <li class="list-inline-item"><a href="#">Termos de Serviço</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
 
   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
